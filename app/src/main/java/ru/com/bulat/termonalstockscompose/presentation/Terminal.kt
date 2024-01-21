4fda5d4e7d260a21451b09d166d5abbfce0dc490
VBulat74 <Vladimir.Bulat@gmail.com>
2024-01-21 21:07:08 +0300

    Lesson_03

--- app/src/main/java/ru/com/bulat/termonalstockscompose/presentation/Terminal.kt
+++ app/src/main/java/ru/com/bulat/termonalstockscompose/presentation/Terminal.kt
@@ -2,21 +2,45 @@ package ru.com.bulat.termonalstockscompose.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import ru.com.bulat.termonalstockscompose.data.Bar
+import kotlin.math.roundToInt
+
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun Terminal(
     bars: List<Bar>,
 ) {
+
+    var visibleBarsCount by remember {
+        mutableStateOf(100)
+    }
+
+    val transformableState = TransformableState { zoomChange, _, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS_COUNT,bars.size)
+//            .coerceAtLeast(MIN_VISIBLE_BARS_COUNT)
+//            .coerceAtMost(bars.size)
+    }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black),
+            .background(Color.Black)
+            .transformable(transformableState),
 
 
         ) {
@@ -25,11 +49,13 @@ fun Terminal(
         val min = bars.minOf { it.low }
 
         val pxPerPoint = size.height / (max - min)
-        val barWidth = size.width / bars.size
+        val barWidth = size.width / visibleBarsCount
 
-        bars.forEachIndexed { index, bar ->
+        bars
+            .take(visibleBarsCount)
+            .forEachIndexed { index, bar ->
 
-            val offsetX = barWidth * index
+            val offsetX = size.width - (barWidth * index)
 
             drawLine(
                 color = Color.White,
