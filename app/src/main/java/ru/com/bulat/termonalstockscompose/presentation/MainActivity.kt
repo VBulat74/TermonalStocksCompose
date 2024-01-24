c33325081dd021fb70d6de823fddf12cc98cdde2
VB <Vladimir.Bulat@gmail.com>
2024-01-24 13:09:38 +0300

    Lesson_06

--- app/src/main/java/ru/com/bulat/termonalstockscompose/presentation/MainActivity.kt
+++ app/src/main/java/ru/com/bulat/termonalstockscompose/presentation/MainActivity.kt
@@ -3,25 +3,23 @@ package ru.com.bulat.termonalstockscompose.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.viewmodel.compose.viewModel
-import ru.com.bulat.termonalstockscompose.ui.theme.TermonalStocksComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TermonalStocksComposeTheme {
-                val viewModel: TerminalViewModel = viewModel()
-                val screenState = viewModel.state.collectAsState()
-                when (val currentState = screenState.value) {
-                    is TerminalScreenState.Content -> {
-                        Terminal(bars = currentState.barList)
-                    }
-
-                    is TerminalScreenState.Initial -> {}
-                }
-            }
+            Test()
+//            TermonalStocksComposeTheme {
+//                val viewModel: TerminalViewModel = viewModel()
+//                val screenState = viewModel.state.collectAsState()
+//                when (val currentState = screenState.value) {
+//                    is TerminalScreenState.Content -> {
+//                        Terminal(bars = currentState.barList)
+//                    }
+//
+//                    is TerminalScreenState.Initial -> {}
+//                }
+//            }
         }
     }
 }
